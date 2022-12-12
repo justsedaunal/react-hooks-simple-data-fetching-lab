876284232d0dccd06581dfc19960566eff8aa648
Seda Ünal <sedaylmz06@outlook.com>
2022-12-12 22:24:41 +0300

    React Simple Data Fetching Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,41 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+import "../App.css";
+
+function App() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+          console.log(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <p>Loading...</p>;
+  } else {
+    return <img src={items.message} alt="A Random Dog" />;
+  }
+}
+
+export default App;
